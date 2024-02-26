63c09e5d54a9070ec38bb8d9b37eecd3b2344338
ScribeQc <93779862+ScribeQc@users.noreply.github.com>
2024-02-26 17:38:02 -0500

    Create lock-schedule
    Lock-schedule and add lock-schedule layout + other front-end adjustements

---
+++ app/src/main/java/com/example/sesamefrontend/classes/ScheduleAdapterList.java
@@ -0,0 +1,24 @@
+package com.example.sesamefrontend.classes;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ScheduleAdapterList extends RecyclerView.Adapter<AdapterList.ViewHolder>{
+    @NonNull
+    @Override
+    public AdapterList.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterList.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
